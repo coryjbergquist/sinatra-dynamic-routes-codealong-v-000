591c358f9827e1bbbbe9adff46ea92210ef903e4
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-07-11 19:47:22 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,9 +20,9 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
 
-  get "/multiply/:number1/:number2" do
+  get "/multiply/:num1/:num2" do
     @num = params[:num]
-    product = 1
+    binding.pry
   end
 
 
